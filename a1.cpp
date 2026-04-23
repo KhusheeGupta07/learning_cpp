ef1cb7ce072c5ec2da751fee06e014098a7d3f28
KhusheeGupta07 <125104049@nitkkr.ac.in>
2026-04-23 20:15:59 +0530

    Add files via upload

---
+++ a1.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+int main(){
+    std::cout<<"ganesha"<<std::endl;
+      std::cout<<"ganesha";
+        std::cout<<"ram"<<"\n";
+          std::cout<<"ram";
+
+
+            std::cout<<"radhe"<< std::endl<<"radhe"  << std::endl<<"radhe"<<std::endl;
+             std::cout<<"ram"<< std::endl<<"ram"  << std::endl<<"ram";
+    return 0;
+}/*#include<iostream>
+using namespace std;
+int main(){
+
+   int age;
+   cin>>age;
+   if(age>=18){ 
+    cout<<"you are an adult";
+   }
+   else {
+    cout<<"not";
+   }
+    return 0;
+}
+*/
