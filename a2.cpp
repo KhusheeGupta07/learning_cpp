0a68c04b011d0de026b623aa8d70ba0600e881cb
KhusheeGupta07 <125104049@nitkkr.ac.in>
2026-04-24 23:17:58 +0530

    Add files via upload

---
+++ a2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+cout<<"ganesha"<<endl;
+      cout<<"ganesha";
+        cout<<"ram"<<"\n";
+          cout<<"ram";
+
+            cout<<"radhe"<< endl<<"radhe"  << endl<<"radhe"<<endl;
+             cout<<"ram"<< endl<<"ram"  << endl<<"ram";
+    return 0;
+}
