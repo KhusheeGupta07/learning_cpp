22d3e8fea4de433c01f1506b133bda1f1118b5f3
KhusheeGupta07 <125104049@nitkkr.ac.in>
2026-04-25 22:50:29 +0530

    Add files via upload

---
+++ a3.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,a,b;
+    cin>>x;
+    cout<<"the value of tha x:"<<x;
+    cin>> a>>b;
+cout<<"the value of tha a:"<<a<<"and b:"<<b;
+return 0;
+}
