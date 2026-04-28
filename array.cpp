de1085dd62ecef6affa24ea4dc5355200bac76fd
KhusheeGupta07 <125104049@nitkkr.ac.in>
2026-04-28 21:43:56 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  /*           //int size; cin>>size;
+    int arr[5];                               //1D array
+    cin>> arr[0]>> arr[1]>>arr[2]>> arr[3] >> arr[4];
+    arr[2] +=2;
+    arr[1] = 51;
+    cout<< arr[3];*/
+
+/*int arr[3][5];
+arr[1][3]='5';
+cout<< arr[1][2];
+return 0;*/
+
+string s = "ganesha";
+int len = s.size();
+cout<<len;
+
+s[len-3]='w';
+cout<<s[6];
+cout<< s[len-1];
+cout<< s[len];
+cout<< s;
+return 0;
+
+}
