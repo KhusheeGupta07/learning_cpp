24941f203e92fa479e7ed863ec7c309ec9d9e52a
KhusheeGupta07 <125104049@nitkkr.ac.in>
2026-05-11 22:26:49 +0530

    Add files via upload

---
+++ datatype.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    long y;
+    long long z;
+    float a;
+    double b;
+    cout<<x<<"\n"<<y<<"\n"<<z<<"\n"<<a<<"\n"<<b;
+
+     string p;
+    getline(cin,p);
+    cout<<p;
+   /* string a;
+    cin>>a;
+    cout<<a<<"\n";
+    string s1,s2;
+    cin>> s1;
+    cin>> s2;
+    cout<<s1<<" "<<s2;*/
+    return 0;
+}
