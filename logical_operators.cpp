a00a30d315a920a7d2f257a8bdb582f291bed620
KhusheeGupta07 <125104049@nitkkr.ac.in>
2026-05-12 22:32:13 +0530

    Add files via upload

---
+++ logical_operators.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+
+    // int a,b,c;
+    // cin>>a>>b>>c;
+
+    // if(a>b && a>c){
+    //     cout<<"yes";
+    // }
+    // else{
+    //     cout<<"no";
+    // }
+    // return 0;
+
+
+
+    // cout<<!5<<endl;     
+    //  cout<<!0<<endl;           // USE OF NOT OPERATOR
+}
