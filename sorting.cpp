75b7ca2e9236b3206c3aa787b8bdfa75cc22852e
KhusheeGupta07 <125104049@nitkkr.ac.in>
2026-05-30 14:13:28 +0530

    Add files via upload

---
+++ sorting.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main(){
+//double arr[5]={600000000,3.84,-5,14,-3};    //INT K ANDER ONLY 9 DIGIT KE NUMBER HI AA SKTE H
+    // for(int i=0;i<4; i++){
+    //     int index = i;
+    //     for(int j=i+1;j<5;j++){
+    //         if(arr[j]<arr[index]){
+    //             index=j; }  }                   // SELECTION SORTING
+    //  swap(arr[i],arr[index])  ;         
+    // }
+    //     cout<<"SORTED_ARRAY:";
+    // for(int i=0;i<5; i++){
+    //   cout<<arr[i]<<"   ";      }
+
+
+// double a[5] ={600000000,3.84,-5,14,-3};
+//      int i,j;
+//   double   t=0;                         //YDI ISE "T " KO INT LE LETE TOH ERROR HO JATA
+//     for(i=0;i<5;i++) {
+//    for (j=0; j<5;j++) {
+//    if(a[j]>a[j+1])   {                  // BUBBLE SORTING
+//     t=a[j];
+//     a[j]=a[j+1];
+//     a[j+1]=t;         }   }  } 
+//   for(i=0;i<5;i++){
+// cout<<a[i]<<"  ";    } 
+
+
+return 0;}
